e2908a434d799d294457e5384cc44d67160b02fe
devil730 <a30sham.07@gmail.com>
2023-02-28 11:43:32 +0530

    app insight

--- src/main/java/com/myapp/employee/EmployeeApplication.java
+++ src/main/java/com/myapp/employee/EmployeeApplication.java
@@ -3,10 +3,13 @@ package com.myapp.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+
 @SpringBootApplication
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
